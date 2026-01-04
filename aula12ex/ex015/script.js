30affe7ca961c07dd96aae6f6f52a75377ea700c
JulioVentorim <julioventorim@gmail.com>
2026-01-04 14:38:10 -0300

    ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,41 @@
-alert('Ola!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0  || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'nenem-H.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-H.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto-H.jpg')
+            } else {
+                img.setAttribute('src', 'idoso-H.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'nenem-M.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-M.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto-M.jpg')
+            } else {
+                img.setAttribute('src', 'idoso-M.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
